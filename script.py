84222e15850f5422d8414e7a87e8e7b16577e2b8
Murtada Ahmed <murtadhabg@gmail.com>
2024-04-14 01:43:00 +0300

    removed hardcoding

--- script.py
+++ script.py
@@ -1,19 +1,11 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 import os
-import json
-import urllib.request
 import time
 import requests
 from PIL import Image
 import io
 import hashlib
 from selenium.webdriver.common.action_chains import ActionChains
-
-from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium import webdriver
@@ -24,6 +16,7 @@ cookies_accept_button_id = 'L2AGLb'
 thumdnail_class_css_selector = 'img.YQ4gaf'
 full_image_class_css_selector = 'img.sFlh5c.pT0Scc.iPVvYb'
 firefox_path = r'C:\Program Files\Mozilla Firefox\firefox.exe'
+
 
 def fetch_image_urls(query, max_links_to_fetch, wd, sleep_between_interactions):
     def scroll_to_end(wd):
@@ -62,7 +55,8 @@ def fetch_image_urls(query, max_links_to_fetch, wd, sleep_between_interactions):
                 continue
 
             try:
-                WebDriverWait(wd, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, full_image_class_css_selector)))
+                WebDriverWait(wd, 10).until(
+                    EC.presence_of_element_located((By.CSS_SELECTOR, full_image_class_css_selector)))
                 actual_images = wd.find_elements(By.CSS_SELECTOR, full_image_class_css_selector)
             except Exception:
                 continue
@@ -70,7 +64,7 @@ def fetch_image_urls(query, max_links_to_fetch, wd, sleep_between_interactions):
             for actual_image in actual_images:
                 if actual_image.get_attribute("src") and "http" in actual_image.get_attribute("src"):
                     image_urls.add(actual_image.get_attribute("src"))
-            
+
             image_count = len(image_urls)
 
             if len(image_urls) >= max_links_to_fetch:
@@ -79,9 +73,10 @@ def fetch_image_urls(query, max_links_to_fetch, wd, sleep_between_interactions):
         else:
             print(f"Found {len(image_urls)} image links, looking for more...")
             time.sleep(30)
-        
+
         result_start = len(thumbnail_results)
     return image_urls
+
 
 def persist_image(folder_path, url):
     try:
@@ -100,12 +95,13 @@ def persist_image(folder_path, url):
     except Exception as e:
         print(f"Error - could not save {url} - {e}")
 
-def search_and_download(search_term, driver_path, number_images=10, target_path="./images" ):
+
+def search_and_download(search_term, driver_path, number_images=10, target_path="./images"):
     target_folder = os.path.join(target_path, "_".join(search_term.lower().split(" ")))
 
     if not os.path.exists(target_folder):
         os.makedirs(target_folder)
-    
+
     # Create a new Firefox service
     s = Service(driver_path)
 
@@ -120,9 +116,8 @@ def search_and_download(search_term, driver_path, number_images=10, target_path=
     for elem in res:
         persist_image(target_folder, elem)
 
+
 keyword_to_search = input("Enter the keyword to search: ")
 number_of_images_to_download = int(input("Enter the number of images to download (default 10): "))
 
-
 search_and_download(keyword_to_search, 'geckodriver.exe', number_of_images_to_download)
-
