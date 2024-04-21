8ac9d957864ac2e83a20cb3fce9b059d13bb50d6
Murtada Ahmed <murtadhabg@gmail.com>
2024-04-21 23:30:18 +0300

    Added secondary images search

--- script.py
+++ script.py
@@ -51,6 +51,7 @@ supported_image_extensions = ['BMP', 'EPS', 'GIF', 'ICNS', 'ICO', 'IM', 'JPEG', 
 need_to_check_secondary_images = True
 secondary_image_button = "/html/body/c-wiz/div[1]/div/div[1]/div[1]/div[2]/div[2]/div[2]/c-wiz/div/div/div/div/div[5]/div/div[1]/a"
 
+
 if getattr(sys, 'frozen', False):
     script_dir = sys._MEIPASS  # If running as executable
 else:
@@ -59,7 +60,7 @@ else:
 geckodriver_path = os.path.join(script_dir, 'geckodriver.exe')
 
 
-def fetch_image_urls(query, max_links_to_fetch, result_start_index, size_filter, target_folder, wd, sleep_between_interactions):
+def fetch_image_urls(query, max_links_to_fetch, result_start_index, size_filter, max_secondary_images, target_folder, wd, sleep_between_interactions):
     def scroll_to_end(wd):
         wd.execute_script("window.scrollTo(0, document.body.scrollHeight);")
         time.sleep(sleep_between_interactions)
@@ -119,45 +120,6 @@ def fetch_image_urls(query, max_links_to_fetch, result_start_index, size_filter,
             try:
                 wd.execute_script("arguments[0].scrollIntoView();", img)
                 ActionChains(wd).move_to_element(img).perform()
-
-
-
-                # ...
-
-                if need_to_check_secondary_images:
-                    ActionChains(wd).key_down(Keys.CONTROL).click(img).key_up(Keys.CONTROL).perform()
-                    time.sleep(sleep_between_interactions)
-                    print(f"{counter}. Clicked + CTRL on {img}")
-                    windows_handles = wd.window_handles
-                    wd.switch_to.window(windows_handles[-1])
-                    print(wd.current_url)
-                    time.sleep(2)
-
-                    # Wait for the element to be present in the DOM, and then scroll it into view
-                    try:
-                        second_button = WebDriverWait(wd, 10).until(
-                            EC.presence_of_element_located((By.XPATH, secondary_image_button)))
-
-                        second_button_url = second_button.get_attribute("href")
-                        print(f"Second button URL: {second_button_url}")
-                        wd.get(second_button_url)
-                        scroll_to_end(wd)
-                        scroll_to_top(wd)
-
-                        print(second_button)
-
-                        # *********************************************
-
-
-                        # *********************************************
-                        wd.close()
-                        wd.switch_to.window(windows_handles[0])
-                    except Exception as e:
-                        print(f"Error finding second button: {e}")
-                        counter += 1
-
-
-
                 img.click()
                 time.sleep(sleep_between_interactions)
                 print(f"{counter}. Clicked on {img}")
@@ -170,7 +132,7 @@ def fetch_image_urls(query, max_links_to_fetch, result_start_index, size_filter,
                 continue
 
             try:
-                actual_image = WebDriverWait(wd, 2).until(
+                actual_image = WebDriverWait(wd, 5).until(
                     EC.presence_of_element_located((By.CSS_SELECTOR, full_image_class_css_selector)))
 
                 img_url = actual_image.get_attribute("src")
@@ -197,8 +159,104 @@ def fetch_image_urls(query, max_links_to_fetch, result_start_index, size_filter,
                 os.startfile(target_folder)
                 break
 
+            # **************************************************************************
+            if need_to_check_secondary_images:
+                ActionChains(wd).key_down(Keys.CONTROL).click(img).key_up(Keys.CONTROL).perform()
+                time.sleep(sleep_between_interactions)
+                print(f"{counter}. Clicked + CTRL on {img}")
+                windows_handles = wd.window_handles
+                wd.switch_to.window(windows_handles[-1])
+                print(wd.current_url)
+                time.sleep(2)
+
+                # Wait for the element to be present in the DOM, and then scroll it into view
+                try:
+                    second_button = WebDriverWait(wd, 10).until(
+                        EC.presence_of_element_located((By.XPATH, secondary_image_button)))
+
+                    second_button_url = second_button.get_attribute("href")
+                    print(f"Second button URL: {second_button_url}")
+                    wd.get(second_button_url)
+                    scroll_to_end(wd)
+                    scroll_to_top(wd)
+
+                    print(second_button)
+
+                    secondary_image_counter = 0
+                    while image_count < max_links_to_fetch:
+                        if secondary_image_counter == max_secondary_images:
+                            break
+                        thumbnail_results2 = wd.find_elements(By.XPATH, thumdnail_class_xpath_selector)
+                        thumbnail_results2 = thumbnail_results2[result_start_index:]
+                        number_results2 = len(thumbnail_results2)
+                        print(f"Found {number_results2} search results in the new page. Extracting links from {result_start_index}:{end_index}")
+
+
+                        for img2 in thumbnail_results2:
+                            try:
+                                wd.execute_script("arguments[0].scrollIntoView();", img2)
+                                ActionChains(wd).move_to_element(img2).perform()
+                                img2.click()
+                                time.sleep(sleep_between_interactions)
+                                print(f"{counter}. Clicked on {img2}")
+
+                                counter += 1
+
+                            except Exception as e:
+                                print(f"{counter}. Error clicking on {img2}: {e}")
+                                counter += 1
+                                continue
+
+                            try:
+                                actual_image2 = WebDriverWait(wd, 2).until(
+                                    EC.presence_of_element_located((By.CSS_SELECTOR, full_image_class_css_selector)))
+
+                                img_url2 = actual_image2.get_attribute("src")
+                                counteven += 1
+                            except Exception as e:
+                                print(f"{counter}. Error finding full image: {e}")
+                                counter += 1
+                                continue
+
+                            print(f"{counter}. Found image: {img_url2}")
+                            download = persist_image(target_folder, img_url2)
+                            if download:
+                                image_urls.add(img_url2)
+
+                            image_count = len(image_urls)
+                            secondary_image_counter += 1
+
+                            print("**************************")
+                            print(f"Image count: {image_count}, max_links_to_fetch: {max_links_to_fetch}")
+                            print("**************************")
+                            if len(image_urls) >= max_links_to_fetch:
+                                print(f"found: {len(image_urls)} image links")
+                                print("**************************")
+                                print("Breaking FOR loop")
+                                os.startfile(target_folder)
+                                break
+                            if secondary_image_counter == max_secondary_images:
+                                print(f"Already downloaded {max_secondary_images} secondary images.")
+                                print("Returning to the main page")
+                                break
+                        else:
+                            time.sleep(2)
+                            print(f"Found {len(image_urls)} image links, looking for more...")
+
+                    wd.close()
+                    wd.switch_to.window(windows_handles[0])
+                    if len(image_urls) >= max_links_to_fetch:
+                        break
+                except Exception as e:
+                    print(f"Error finding second button: {e}")
+                    counter += 1
+            else:
+                print("Not checking secondary images")
+            # **************************************************************************
+            if len(image_urls) >= max_links_to_fetch:
+                break
         else:
-            time.sleep(300)
+            time.sleep(2)
             print(f"Found {len(image_urls)} image links, looking for more...")
 
         # scroll_to_end(wd)
@@ -242,7 +300,7 @@ def persist_image(folder_path, url):
         return False
 
 
-def search_and_download(search_term, driver_path, number_images, result_start, size_filter, target_path="./images"):
+def search_and_download(search_term, driver_path, number_images, result_start, size_filter,max_secondary_images, target_path="./images"):
     target_folder = os.path.join(target_path, "_".join(search_term.lower().split(" ")))
 
     if not os.path.exists(target_folder):
@@ -257,7 +315,7 @@ def search_and_download(search_term, driver_path, number_images, result_start, s
     # options.add_argument("-headless")
     # Create a new instance of the Firefox driver
     with webdriver.Firefox(options=options, service=s) as wd:
-        fetch_image_urls(search_term, number_images, result_start, size_filter, target_folder, wd=wd,
+        fetch_image_urls(search_term, number_images, result_start, size_filter,max_secondary_images, target_folder, wd=wd,
                          sleep_between_interactions=0.5)
 
     # for elem in res:
@@ -282,7 +340,12 @@ while not result_start.isdigit():
 image_size = input("Enter the image size (l: large, m: medium, i: icon, Enter: default): ")
 while image_size not in ["l", "m", "i", ""]:
     image_size = input("You must enter a valid image size (l: large, m: medium, i: icon, Enter: default): ")
+
+max_secondary_images = input("Enter the maximum number of secondary images: ")
+while not max_secondary_images.isdigit() or not max_secondary_images:
+    print("You must enter a number for maximum secondary images")
+
 search_and_download(keyword_to_search, geckodriver_path, int(number_of_images_to_download), int(result_start),
-                    image_size)
+                    image_size, int(max_secondary_images))
 input("Press Enter to exit...")
 
