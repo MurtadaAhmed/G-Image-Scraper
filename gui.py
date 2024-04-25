c876e9869106a24354c2210a77648cfbd202a1f1
Murtada Ahmed <murtadhabg@gmail.com>
2024-04-25 19:47:34 +0300

    gui

--- gui.py
+++ gui.py
@@ -1,8 +1,17 @@
-# gui.py
 from tkinter import Tk, Label, Button, Entry, StringVar, Text, END
-from script import search_and_download, geckodriver_path
+from script import search_and_download, check_imagemagick_dependency, main_inputs, geckodriver_path
+import sys
 
-from tkinter import Tk, Label, Button, Entry, StringVar, Text, END
+class TextRedirector(object):
+    def __init__(self, widget):
+        self.widget = widget
+
+    def write(self, str):
+        self.widget.insert(END, str)
+        self.widget.see(END)
+
+    def flush(self):
+        pass
 
 class ImageScraperApp(Tk):
     def __init__(self):
@@ -38,15 +47,38 @@ class ImageScraperApp(Tk):
 
         self.output_text = Text(self)
         self.output_text.pack()
+        self.secondary_label = Label(self, text="Secondary images")
+        self.secondary_label.pack()
+        self.secondary = StringVar()
+        self.secondary_entry = Entry(self, textvariable=self.secondary)
+        self.secondary_entry.pack()
+
+        self.headless_label = Label(self, text="Headless mode (y/n)")
+        self.headless_label.pack()
+        self.headless = StringVar()
+        self.headless_entry = Entry(self, textvariable=self.headless)
+        self.headless_entry.pack()
+
+        self.search_button = Button(self, text="Search", command=self.search_images)
+        self.search_button.pack()
+
+        self.output_text = Text(self)
+        self.output_text.pack()
+
+        sys.stdout = TextRedirector(self.output_text)
 
     def search_images(self):
         keyword = self.keyword.get()
         number = int(self.number.get())
         start = int(self.start.get())
         size = self.size.get()
-        search_and_download(keyword, geckodriver_path, number, start, size)
+        secondary = int(self.secondary.get())
+        headless = self.headless.get() == 'y'
+        search_and_download(keyword, geckodriver_path, number, start, size, secondary, headless)
         self.output_text.insert(END, "Search completed.\n")
+
 
 if __name__ == "__main__":
     app = ImageScraperApp()
     app.mainloop()
+
